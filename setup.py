a59d2adc6f3184278a9ff659bea3b2291c7d4aa5
KALPESHNIKUMBH <kalpeshnikumbh811@gmail.com>
2023-02-13 15:40:19 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+
+setup(
+    name = "sensor",
+    version = "0.0.1",
+    author = "kalpesh nikumbh",
+    author_email = "kalpeshnikumbh999@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements(),
+)
