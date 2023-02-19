b3591e7e08dd19e3fa74500b613007d0c40f9c55
KALPESHNIKUMBH <kalpeshnikumbh999@gmail.com>
2023-02-19 15:41:07 +0530

    new update

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-from sensor.logger import logging
-from sensor.exception import SensorException
 import sys,os
-
-def test_logger_and_exception():
-     try:
-          logging.info("starting the test_logger_and_exception")
-          result = 3/00
-          print(result)
-          logging.info("stopping the test_logger_and_exception")
-     except Exception as e:
-          logging.debug("stopping the test_logger_and_exception")
-          raise SensorException(e, sys)
-
 
 if __name__=="__main__":
      try:
