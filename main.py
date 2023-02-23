e682ffc6bdc7912fa27bdf741e70000547dada17
KALPESHNIKUMBH <kalpeshnikumbh999@gmail.com>
2023-02-23 16:03:56 +0530

    UPDATE

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
-import os,sys
-from sensor.exception import SensorException
-from sensor.logger import logging
-from datetime import datetime
-from sensor.utils import get_collection_as_dataframe
-from sensor.entity.config_entity import DataIngestionConfig
+
+
+import sys,os
 from sensor.entity import config_entity
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
 
-'''
+print(__name__)
 if __name__=="__main__":
-    try:
-        training_pipeline_config = config_entity.TrainingPipelineConfig()
-        data_ingestion_config =DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-        print(data_ingestion_config.to_dict())
-    except Exception as e:
-        print(e)
+     try:
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
 
-'''
+          data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+          data_validation = DataValidation(data_validation_config=data_validation_config,
+                         data_ingestion_artifact=data_ingestion_artifact)
 
-
-if __name__=="__main__":
-    try:
-        training_pipeline_config = config_entity.TrainingPipelineConfig()
-        data_ingestion_config =config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-        print(data_ingestion_config.to_dict())
-        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-        print(data_ingestion.initiate_data_ingestion())
-    except Exception as e:
-        print(e)
+          data_validation_artifact = data_validation.initiate_data_validation()
+     except Exception as e:
+          print(e)
--- sensor/utils.py
+++ sensor/utils.py
@@ -4,9 +4,6 @@ from sensor.exception import SensorException
 from sensor.config import mongo_client
 import os,sys
 import yaml
-import numpy as np
-import dill
-
 def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
     """
     Description: This function return collection as dataframe
@@ -28,3 +25,22 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
         return df
     except Exception as e:
         raise SensorException(e, sys)
+
+
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
+    except Exception as e:
+        raise SensorException(e, sys)
+
+def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
+    try:
+        for column in df.columns:
+            if column not in exclude_columns:
+                df[column]=df[column].astype('float')
+        return df
+    except Exception as e:
+        raise e
